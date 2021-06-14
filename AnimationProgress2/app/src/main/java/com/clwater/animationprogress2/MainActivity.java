2910b7febc2b6085f44be77f5167253b58f053e3
clwater <gengzhibo@gengzhibo.net>
2021-06-14 19:11:54 +0800

    WIP(code): 展示功能

--- AnimationProgress2/app/src/main/java/com/clwater/animationprogress2/MainActivity.java
+++ AnimationProgress2/app/src/main/java/com/clwater/animationprogress2/MainActivity.java
@@ -4,6 +4,9 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,17 +20,42 @@ public class MainActivity extends AppCompatActivity {
 
     private void initView() {
         AnimationProgressTip animationProgressTip = findViewById(R.id.ap_main);
-        findViewById(R.id.bt_test).setOnClickListener(new View.OnClickListener() {
+        EditText editProgress = findViewById(R.id.et_progress);
+
+        //设置进度(100)
+        findViewById(R.id.bt_progress_100).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 animationProgressTip.setProgress(100);
             }
         });
-        findViewById(R.id.bt_test_2).setOnClickListener(new View.OnClickListener() {
+
+        //设置进度(0)
+        findViewById(R.id.bt_progress_0).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 animationProgressTip.setProgress(0);
             }
+        });
+
+        //设置进度(自定义)
+        findViewById(R.id.bt_progress_set).setOnClickListener(v -> {
+            int progress = Integer.parseInt(editProgress.getText().toString());
+            if (progress > 100) {
+                progress = 100;
+            } else if (progress < 0) {
+                progress = 0;
+            }
+            editProgress.setText("" + progress);
+            animationProgressTip.setProgress(progress);
+        });
+
+        //设置进度(随机)
+        findViewById(R.id.bt_progress_random).setOnClickListener(v -> {
+            int progress = new Random().nextInt(100);
+            editProgress.setText("" + progress);
+
+            animationProgressTip.setProgress(progress);
         });
     }
 }
